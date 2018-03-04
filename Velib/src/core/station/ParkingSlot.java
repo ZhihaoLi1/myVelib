08c7b7a5658c3452589e7500f40760057bf4acd6
Yiren Wang <yiren.wang@student.ecp.fr>
2018-03-05 00:14:46 +0100

    feat(Station/parkingslot): add getters and setters

--- Velib/src/core/station/ParkingSlot.java
+++ Velib/src/core/station/ParkingSlot.java
@@ -1,4 +1,33 @@
 package core.station;
 
+import core.bike.Bike;
+
 public class ParkingSlot {
+	private final int id;
+	private Boolean working = true;
+	private Bike bike;
+	
+	public ParkingSlot(){
+		this.id = IDGenerator.getInstance().getNextIDNumber();
+	}
+
+	public Boolean getWorking() {
+		return working;
+	}
+
+	public void setWorking(Boolean working) {
+		this.working = working;
+	}
+
+	public Bike getBike() {
+		return bike;
+	}
+
+	public void setBike(Bike bike) {
+		this.bike = bike;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
