82af551aa38648258ec98bc6c99ce73f5f8b1148
Yiren Wang <yiren.wang@student.ecp.fr>
2018-03-04 16:23:57 +0100

    feat(Network): populate attributes and methods

--- Velib/src/core/Network.java
+++ Velib/src/core/Network.java
@@ -1,5 +1,27 @@
 package core;
 
-public class Network {
+import java.util.HashMap;
 
+import core.point.Point;
+import core.rentals.BikeRental;
+import core.ridePlan.RidePlan;
+import core.ridePlan.RidePlanStrategy;
+import core.station.Station;
+
+public class Network implements RidePlanStrategy {
+	private HashMap<Integer, Station> stations; 
+	private HashMap<Integer, User> users;
+	private HashMap<User, BikeRental> userRentals;
+	private HashMap<User, RidePlan> userRidePlans;
+
+	public Network(){
+		
+	}
+	
+	@Override
+	public Station[] planRide(Point source, Point destination) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
