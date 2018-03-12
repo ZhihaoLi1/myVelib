24332beb071d80d4ddf18f605dc0b13bb9c4d3e4
Yiren Wang <yiren.wang@student.ecp.fr>
2018-03-12 21:08:34 +0100

    feat(RidePlan): add fastest plan and combine test set up for fastest and shortest plan

--- Velib/src/core/ridePlan/FastestPlan.java
+++ Velib/src/core/ridePlan/FastestPlan.java
@@ -1,6 +1,7 @@
 package core.ridePlan;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import core.User;
 import core.point.Point;
@@ -9,9 +10,50 @@ import core.station.Station;
 public class FastestPlan implements RidePlanStrategy{
 
 	@Override
-	public RidePlan planRide(Point source, Point destination, User user, String policy, String bikeType, HashMap<Integer, Station> stations) {
-		// TODO Auto-generated method stub
-		return null;
+	public RidePlan planRide(Point source, Point destination, User user, String bikeType, HashMap<Integer, Station> stations) throws Exception {
+		double walkingSpeed = 4; // km/h
+		double mechBikeSpeed = 15;
+		double elecBikeSpeed = 20;
+		
+		Station sourceStation = null;
+		Station destStation = null;
+		double minimumTime = 1000000000; //hours
+		// different possible pairs 
+		for (Map.Entry<Integer, Station> entry : stations.entrySet()) {
+			// source station
+		    Integer stationId1 = entry.getKey();
+		    Station s1 = entry.getValue();
+		    if(bikeType == "elec") {
+			    if(!s1.hasElecBike()) continue;
+		    } else if(bikeType == "mech") {
+			    if(!s1.hasMechBike()) continue;
+		    }
+			for (Map.Entry<Integer, Station> entry2 : stations.entrySet()) {
+				// dest Station 				
+			    Integer stationId2 = entry2.getKey();
+			    Station s2 = entry2.getValue();
+			    if (!s2.equals(s1) && !s2.isFull()) {
+				    double totalTime = 0;
+				    totalTime += s1.getCoordinates().distance(source)/walkingSpeed;
+				    if(bikeType == "elec") {
+					    totalTime += s1.getCoordinates().distance(s2.getCoordinates())/elecBikeSpeed;
+				    } else if (bikeType == "mech"){
+					    totalTime += s1.getCoordinates().distance(s2.getCoordinates())/mechBikeSpeed;
+				    }
+				    totalTime += s2.getCoordinates().distance(destination)/walkingSpeed;
+				    
+				    if (totalTime < minimumTime) {
+				    		sourceStation = s1;
+				    		destStation = s2;
+				    		minimumTime = totalTime;
+				    }
+			    }
+			}
+		}
+		if (sourceStation == null || destStation == null) {
+			throw new Exception("No appropriate stations found !");
+		}
+		return new RidePlan(source, destination, sourceStation, destStation, "shortest", bikeType);
 	}
 
 }
---
+++ Velib/src/core/ridePlan/RidePlanShortestOrFastestPathTest.java
@@ -0,0 +1,82 @@
+package core.ridePlan;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import core.Network;
+import core.User;
+import core.bike.ElecBike;
+import core.bike.MechBike;
+import core.card.NoCard;
+import core.point.Point;
+import core.station.StandardStation;
+import core.station.Station;
+
+public class RidePlanShortestOrFastestPathTest {
+
+	// create a network
+	static Network n = new Network();
+	// Create source, destination
+	Point source = new Point(0,0);
+	Point destination = new Point(10,10);
+	// Create User
+	User bob = new User("bob", new Point(0,0), new NoCard());
+	
+	// Create source stations 
+	static Station sourceStationS = new StandardStation(10, new Point(2,2));
+	// Create dest stations 
+	static Station destStationS = new StandardStation(10, new Point(8, 8));
+
+	// Create plus source stations 
+	static Station sourceStationF = new StandardStation(10, new Point(0,2));
+	// Create standard dest stations 
+	static Station destStationF = new StandardStation(10, new Point(8,10));
+
+    @BeforeClass
+    public static void initialize() {
+		try {
+
+			n.addStation(sourceStationS);
+			n.addStation(destStationS);
+			n.addStation(sourceStationF);
+			n.addStation(destStationF);
+			
+			// add one bike to source and destination stations : They are all Mechanical. 
+			sourceStationS.addBike(new MechBike());
+			destStationS.addBike(new MechBike());
+			sourceStationF.addBike(new MechBike());
+			destStationF.addBike(new MechBike());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+    }
+
+	@Test
+	/**
+	 * Choose a the source station as source station and dest station as dest station not the other way round
+	 */
+	public void chooseCorrectStationsWhenPlanningShortestRide() {		
+		RidePlan s = n.planRide(source, destination, bob, "shortest", "Mech");
+		RidePlan sRidePlan = new RidePlan(source, destination, sourceStationS, destStationS, "shortest", "Mech");
+		System.out.println(s.getSourceStation().getId());
+		System.out.println(s.getDestinationStation().getId());
+		System.out.println(sourceStationS.getId());
+		System.out.println(destStationS.getId());
+		assertTrue(s.equals(sRidePlan));
+	}
+
+	public void chooseCorrectStationsWhenPlanningFastestRide() {		
+		RidePlan s = n.planRide(source, destination, bob, "shortest", "Mech");
+		RidePlan sRidePlan = new RidePlan(source, destination, sourceStationF, destStationF, "shortest", "Mech");
+		System.out.println(s.getSourceStation().getId());
+		System.out.println(s.getDestinationStation().getId());
+		System.out.println(sourceStationS.getId());
+		System.out.println(destStationS.getId());
+		assertTrue(s.equals(sRidePlan));
+	}
+
+}
