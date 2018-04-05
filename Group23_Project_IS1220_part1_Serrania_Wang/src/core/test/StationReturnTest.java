90054077bdc8529621131f5fdea392b98885a551
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-04-05 13:38:04 +0200

    Test(Station): Add tests for returnBike and equals (for stations)

---
+++ Group23_Project_IS1220_part1_Serrania_Wang/src/core/test/StationReturnTest.java
@@ -0,0 +1,111 @@
+package core.test;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import core.Network;
+import core.bike.Bike;
+import core.bike.BikeFactory;
+import core.bike.InvalidBikeTypeException;
+import core.card.CardVisitorFactory;
+import core.card.InvalidCardTypeException;
+import core.rentals.BikeRental;
+import core.station.FullStationException;
+import core.station.InvalidStationTypeException;
+import core.station.Station;
+import core.station.StationFactory;
+import core.user.User;
+import utils.DateParser;
+import utils.Point;
+
+public class StationReturnTest {
+
+	// create a network
+	static Network n = new Network("EmptyNetwork", 0, 0, 4, 0, 0, 0, LocalDateTime.now());
+	// Create User
+	static User bob, alice;
+
+	static StationFactory stationFactory = new StationFactory();
+	static BikeFactory bikeFactory = new BikeFactory();
+	static CardVisitorFactory cardVisitorFactory = new CardVisitorFactory();
+
+	// Stations
+	static Station s, emptyS, fullS;
+
+	@Before
+	public void fillStationAndNetwork() {
+		try {
+			s = stationFactory.createStation("PLUS", 10, new Point(0, 0.1), true);
+			emptyS = stationFactory.createStation("PLUS", 10, new Point(0, 0.2), true);
+			fullS = stationFactory.createStation("PLUS", 0, new Point(0, 0.2), true);
+		} catch (InvalidStationTypeException e) {
+			fail("InvalidStationTypeException was thrown");
+		}
+
+		try {
+			bob = new User("Bob", new Point(0, 0), cardVisitorFactory.createCard("NO_CARD"));
+			alice = new User("Alice", new Point(5, 2), cardVisitorFactory.createCard("NO_CARD"));
+		} catch (InvalidCardTypeException e) {
+			fail("InvalidCardTypeException was thrown");
+		}
+
+		// add bikes to stations
+		try {
+			s.addBike(bikeFactory.createBike("MECH"), LocalDateTime.now());
+			s.addBike(bikeFactory.createBike("MECH"), LocalDateTime.now());
+			s.addBike(bikeFactory.createBike("ELEC"), LocalDateTime.now());
+		} catch (InvalidBikeTypeException e) {
+			fail("InvalidBikeTypeException was thrown");
+		}
+
+		// add user and station to network
+		n.addUser(bob);
+		n.addStation(s);
+		n.addStation(emptyS);
+	}
+
+	@Test
+	public void stationHasOneMoreBikeOfTheDesiredKindAfterReturn() {
+
+		Bike b = s.rentBike("MECH", DateParser.parse("01/01/2000 09:00:00"));
+		try {
+			emptyS.returnBike(new BikeRental(b, DateParser.parse("01/01/2000 09:00:00")),
+					DateParser.parse("01/01/2000 10:00:00"));
+		} catch (FullStationException e) {
+			fail("FullStationException thrown");
+		}
+		int numMechBikes = emptyS.getNumberOfBikes("MECH");
+		int numElecBikes = emptyS.getNumberOfBikes("ELEC");
+		assertEquals(1, numMechBikes);
+		assertEquals(0, numElecBikes);
+
+		b = s.rentBike("ELEC", DateParser.parse("01/01/2000 10:00:00"));
+		try {
+			emptyS.returnBike(new BikeRental(b, DateParser.parse("01/01/2000 10:00:00")),
+					DateParser.parse("01/01/2000 11:00:00"));
+		} catch (FullStationException e) {
+			fail("FullStationException thrown");
+		}
+		numMechBikes = emptyS.getNumberOfBikes("MECH");
+		numElecBikes = emptyS.getNumberOfBikes("ELEC");
+		assertEquals(1, numMechBikes);
+		assertEquals(1, numElecBikes);
+	}
+
+	@Test
+	public void whenStationIsFullThenThrowException() {
+		Bike b = s.rentBike("ELEC", DateParser.parse("01/01/2000 10:00:00"));
+		try {
+			fullS.returnBike(new BikeRental(b, DateParser.parse("01/01/2000 10:00:00")),
+					DateParser.parse("01/01/2000 11:00:00"));
+			fail("FullStationException should have been thrown");
+		} catch (FullStationException e) {
+			assertTrue(true);
+		}	
+	}
+
+}
---
+++ Group23_Project_IS1220_part1_Serrania_Wang/src/core/test/StationTest.java
@@ -0,0 +1,35 @@
+package core.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import core.station.InvalidStationTypeException;
+import core.station.StandardStation;
+import core.station.Station;
+import core.station.StationFactory;
+import utils.Point;
+
+public class StationTest {
+
+	@Test
+	public void testEqualsObject() {
+		StationFactory stationFactory = new StationFactory();
+		Station s1 = null;
+		Station s2 = null;
+		Station s3 = null;
+		try {
+			s1 = stationFactory.createStation("STANDARD", 0, new Point(0, 0), true);
+			s2 = stationFactory.createStation("STANDARD", 0, new Point(0, 2), true);
+			s3 = stationFactory.createStation("PLUS", 0, new Point(3, 1), true);
+		} catch (InvalidStationTypeException e) {
+			fail("InvalidStationTypeException thrown");
+		}
+		
+		assertTrue(s1.equals(s1));
+		assertTrue(!s1.equals(s2));
+		assertTrue(!s1.equals(s3));
+		assertTrue(s3.equals(s3));
+	}
+
+}
